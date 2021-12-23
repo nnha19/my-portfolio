9e785b4ce4d3ecddac5be2a3cbe86e44d08799f0
nnha19 <nyinyihmueaung19@gmail.com>
2021-12-23 20:04:56 +0630

    desktop navbar finishes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
     <nav className="nav wrapper">
       <h1 className="nav__logo">Logo</h1>
-      <ul className="nav__lists">
+
+      <ul className={`nav__lists ${!showNav ? "nav-hidden" : undefined} `}>
         <li className="nav__list nav__item">Projects</li>
         <li className="nav__list nav__item">About</li>
         <li className="nav__list nav__item">Contact</li>
-        <li className="nav__list">
+        <li className="nav__list github">
           <a href="https://github.com/nnha19" target="_black">
             <i className="fab fa-github"></i>
           </a>
         </li>
         <li className="nav__list">
-          <button className="nav__btn">Resume</button>
+          <a
+            href="https://drive.google.com/file/d/1T8jkspLIFrmAhjsBGTeyv5FeMW6KfQFG/view?usp=sharing"
+            target="_blank"
+          >
+            <button className="nav__btn">Resume</button>
+          </a>
         </li>
       </ul>
+      {showNav && (
+        <a
+          className="nav__github"
+          href="https://github.com/nnha19"
+          target="_black"
+        >
+          <i className="fab fa-github"></i>
+        </a>
+      )}
+      <div onClick={() => setShowNav(true)} className="hamburger-container">
+        <span className="hamburger">Hamburger</span>
+      </div>
     </nav>
   );
 };
