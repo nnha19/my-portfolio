ef225ef59e8ee19f71478135ead6e2922e0266ef
nnha19 <nyinyihmueaung19@gmail.com>
2021-12-23 13:20:29 +0630

    skills section finishes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import Skills from "./components/Skills/Skills";
 
 const App = () => {
   return (
     <div className="container">
       <Home />
+      <Skills />
     </div>
   );
 };
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Skills.scss";
+const Skills = () => {
+  const mySkills =
+    "html css javascript react redux typescript scss node express ";
+
+  return (
+    <div className="skills wrapper">
+      <h2 className="skills__header">My Skills</h2>
+      <p>
+        As of now, these are the skills I have been working with and I am open
+        and eager to learn any new skills required.
+      </p>
+      <ul className="skill-list">
+        <div>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>HTML</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>CSS/Scss</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Javascript</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>React/Redux</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Next.js</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Typescript</p>
+          </li>
+        </div>
+        <div>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Node.js</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Express</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>MongoDB</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>JWT</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Git/Github</p>
+          </li>
+          <li className="skill">
+            <i class="far fa-check-circle"></i>
+            <p>Figma</p>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
